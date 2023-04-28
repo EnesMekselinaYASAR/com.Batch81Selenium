390af798d9a2d44c320da5693fba99c8ba74a61a
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-28 23:30:54 +0300

    28.04.2023

--- src/test/java/day01_seleniumProje/C01_IlkClass.java
+++ src/test/java/day01_seleniumProje/C01_IlkClass.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class C01_IlkClass {
     public static void main(String[] args) throws InterruptedException {
+
         /*
         En ilkel haliyle bir otomasyon yapmak icin
         Class'imiza otomasyon icin gerekli webdriver'in yerini gostermemiz gerekir
@@ -13,6 +14,7 @@ public class C01_IlkClass {
         ilki kullanacaginiz driver: webdriver.chrome.driver
         ikincisi ise bu driverin fiziki yolu :
          */
+
         System.setProperty("webdriver.chrome.driver","src/resources/drives/chromedriver.exe");
         WebDriver driver=new ChromeDriver();
 
