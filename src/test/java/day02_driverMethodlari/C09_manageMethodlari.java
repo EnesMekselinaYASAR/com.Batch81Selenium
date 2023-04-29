df4de38d4b3cb7dc7eba7b40dc9731b69d65ec1d
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-29 17:39:05 +0300

    29.04.2023

--- src/test/java/day02_driverMethodlari/C09_manageMethodlari.java
+++ src/test/java/day02_driverMethodlari/C09_manageMethodlari.java
@@ -1,4 +1,28 @@
 package day02_driverMethodlari;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C09_manageMethodlari {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","src/resources/drives/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.amazon.com");
+
+        /*
+        Bir sayfa açılırken ilk başta sayfanın içerisinde bulunan
+        elementlere göre bir yüklenme süresine ihtiyaç vardır.
+        veya bir webelementin kullanılabilmesi için zamana ihtiyaç olabilir.
+
+        implicitlyWait bize sayfanın yüklenmesi ve sayfadaki elementlere ulaşım için
+        beklenecek MAXIMUM sureyi belirleme olanağı tanır.
+         */
+
+
+    }
 }
