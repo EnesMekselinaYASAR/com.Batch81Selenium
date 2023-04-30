f092be23ef31a8f91142de0d7a19348af9a4563c
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-30 11:39:22 +0300

    30.04.2023

---
+++ src/test/java/day04_xpath_cssLocator/C03_CssLocator.java
@@ -0,0 +1,22 @@
+package day04_xpath_cssLocator;
+
+public class C03_CssLocator {
+    public static void main(String[] args) {
+
+        /*
+1 ) Bir class oluşturun : Locators_css
+2)Main method oluşturun ve aşağıdaki görevi tamamlayın.
+a. Verilen web sayfasına gidin. http://a.testaddressbook.com/sign_in
+b. Locate email textbox
+c. Locate password textbox ve
+d. Locate signin button
+e. Asagidaki kullanıcı adını ve şifreyi girin ve sign in düğmesini
+tıklayın
+i. Username : testtechproed@gmail.com
+ii. Password : Test1234!
+NOT: cssSelector kullanarak elementleri locate ediniz.
+*/
+
+        System.setProperty("webdriver.chrome.driver","src/resources/drives/chromedriver.exe");
+    }
+}
