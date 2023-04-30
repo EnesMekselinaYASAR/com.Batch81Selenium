18bbcc70ca34cb430c9aad885c9710a873e17beb
Enes Mekselina Yaşar <av.enesmyasar@gmail.com>
2023-04-30 10:32:27 +0300

    30.04.2023

---
+++ src/test/java/day04_xpath_cssLocator/C01_AmazonSearch.java
@@ -0,0 +1,41 @@
+package day04_xpath_cssLocator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class C01_AmazonSearch {
+        /*
+        1-Main method oluşturun ve aşağıdaki görevi tamamlayın
+        a. https://amazon.com sayfasına gidin.
+        b. Search "city bike"
+        c. Amazonda görüntülenen ilgili sonuçların görüntülerini yazdırın
+        d. Sonra karşınıza çıkan ilk sonucun resmine tıklayın.
+         */
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resources/drives/chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://www.amazon.com");
+
+        WebElement aramaKutusu=driver.findElement(By.id("twotabsearchtextbox"));
+
+        aramaKutusu.sendKeys("city bike"+ Keys.ENTER);
+
+        List<WebElement> istenenElementListesi=driver.findElements(By.className("sg-col-inner"));
+        WebElement sonucYazisiElementi=istenenElementListesi.get(0);
+        System.out.println(sonucYazisiElementi.getText()); // 1-16 of 251 results for "city bike"
+
+        List<WebElement> urunResimleriList=driver.findElements(By.className("s-image"));
+        WebElement ilkUrunResmi=urunResimleriList.get(0);
+        ilkUrunResmi.click();
+
+    }
+}
